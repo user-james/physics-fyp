04acfb8c9bdaa271030853c03bee01e2c895ddee
James Lab <j.delaney@yahoo.com>
2017-04-03 17:14:15 +0100

    editing plotting programs to get data for final write up

--- plotter.py
+++ plotter.py
@@ -1,31 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 
 
 if __name__=='__main__':
 
-    data = np.genfromtxt("tmvalues1.343061.txt")
+    data = np.genfromtxt("./modes/values1.355118.txt")
     r, a, x, y = data
 
 
     c = np.exp(y)*np.cos(x)
-
+    print(c)
     mu, gamma = x/a, y/a
 
     x1 = np.linspace(-2*a, -a, 100)
     x2 = np.linspace(-a, a, 200)
     x3 = np.linspace(a, 2*a, 100)
+    
+    fig, ax = plt.subplots()
 
+    ax.plot(x1, c*np.exp(gamma*x1), 'b-')
+    ax.plot(x3, c*np.exp(-gamma*x3), 'b-')
+    ax.plot(x2, np.cos(mu*x2), 'r-')
 
-    plt.plot(x1, c*np.exp(gamma*x1), 'b-')
-    plt.plot(x3, c*np.exp(-gamma*x3), 'b-')
-    plt.plot(x2, np.cos(mu*x2), 'r-')
+    ax.plot(-a*np.ones(100), np.linspace(-1.5, 1.5, 100), 'k--')
+    ax.plot(a*np.ones(100), np.linspace(-1.5, 1.5, 100), 'k--')
 
-    plt.plot(-a*np.ones(100), np.linspace(-1.5, 1.5, 100), 'k--')
-    plt.plot(a*np.ones(100), np.linspace(-1.5, 1.5, 100), 'k--')
-
-    #plt.ylim(0, 1.2)
-    plt.savefig("TM1.jpeg")
+    ax.set_ylim(0, 1.2)
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$E_y$')
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%.1e'))
+    plt.savefig("./plots/TE0.jpeg")
     plt.show()
 
